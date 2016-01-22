1957e9f9dcd2787e135ea7eafde166fb32bd36a1
Yogesh Chawla <yogesh@search.org>
2016-01-22 10:23:23 -0600

    Added script to create indexes on columns to improve performance with larger data sets.

---
+++ incident-arrest/db/incident_arrest_indexes.sql
@@ -0,0 +1,3 @@
+CREATE INDEX `idx_incident_IncidentDateTime`  ON `ojbc_analytics`.`incident` (IncidentDateTime);
+CREATE INDEX `idx_agency_AgencyName`  ON `ojbc_analytics`.`agency` (AgencyName);
+CREATE INDEX `idx_town_TownDescription`  ON `ojbc_analytics`.`town` (TownDescription);
