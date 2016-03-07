e0bf0a915ef9c7bb6139c6ce3d5cca1146dede94
Scott Came <scott@search.org>
2016-03-07 15:19:43 -0800

    Add ggthemes theming to dashboard

--- r-packages/OJBDemoDashboard/R/MakeCharts.R
+++ r-packages/OJBDemoDashboard/R/MakeCharts.R
@@ -1,3 +1,10 @@
+#' Internal function for controlling the theme in one place
+#' 
+#' @import ggthemes
+getTheme <- function() {
+  ggthemes::theme_gdocs()
+}
+
 #' Create the chart for r1, c1
 #'
 #' @import ggplot2
@@ -6,11 +13,12 @@
 #' @export
 chart_r1c1 <- function() {
   svglite("chart_r1c1.svg")
-  print(ggplot(data=YearData, aes(x=yearID)) +
+  plot <- ggplot(data=YearData, aes(x=yearID)) +
     geom_line(aes(y=Avg, color="Average")) +
     geom_line(aes(y=KRate, color="Strikeout Rate")) +
-    xlab("Year") +
-    theme(axis.title.y = element_blank(), legend.title=element_blank()))
+    xlab("Year") + getTheme() +
+    theme(axis.title.y = element_blank(), legend.title=element_blank())
+  print(plot)
   dev.off()
   invisible()
 }
@@ -30,8 +38,9 @@ chart_r1c2 <- function(league) {
     filter(opp %in% leagueChoice)
   svglite("chart_r1c2.svg")
   print(ggplot(data=df, aes(x=team, y=opp)) + geom_tile(aes(fill=Avg)) +
-    scale_fill_gradient(low = "red", high = "green") + xlab("Team") + ylab("Opponent"))
+    scale_fill_gradient(low = "red", high = "green") + xlab("Team") + ylab("Opponent") + getTheme())
   dev.off()
+  invisible()
 }
 #' Create the chart for r1, c3
 #'
@@ -43,8 +52,9 @@ chart_r1c2 <- function(league) {
 chart_r1c3 <- function() {
   svglite("chart_r1c3.svg")
   print(ggplot(data=TeamData2015, aes(x=Wins, y=RunDiff)) + geom_point() + geom_text_repel(aes(label=team), nudge_x=1) +
-          xlab("Season Wins") + ylab("Total Run Differential"))
+          xlab("Season Wins") + ylab("Total Run Differential") + getTheme())
   dev.off()
+  invisible()
 }
 #' Create the chart for r2, c1
 #'
@@ -55,8 +65,9 @@ chart_r1c3 <- function() {
 chart_r2c1 <- function() {
   wins <- GameData2015 %>% filter(win==1) %>% mutate(TotalRuns=Runs+RunsAllowed)
   svglite("chart_r2c1.svg")
-  print(ggplot(data=wins, aes(x=TotalRuns)) + geom_bar() + xlab("Total Runs Scored") + ylab("# of games"))
+  print(ggplot(data=wins, aes(x=TotalRuns)) + geom_bar() + xlab("Total Runs Scored") + ylab("# of games") + getTheme())
   dev.off()
+  invisible()
 }
 #' Create the chart for r2, c2
 #'
@@ -74,7 +85,8 @@ chart_r2c2 <- function() {
     arrange(desc(var))
   svglite("chart_r2c2.svg")
   print(ggplot(data=ps2, aes(x=reorder(label, -value), y=value, fill=var)) + geom_bar(stat="identity") +
-          xlab("Player/Season") + ylab("# of Home Runs") + scale_fill_discrete(labels=c("Player HR", "95th %ile HR")) +
+          xlab("Player/Season") + ylab("# of Home Runs") + scale_fill_discrete(labels=c("Player HR", "95th %ile HR")) + getTheme() +
           theme(axis.text.x = element_text(angle = 60, hjust = 1), legend.title=element_blank()))
   dev.off()
+  invisible()
 }
