9e6198d046f7f013586ef4c2204f075811e8470c
andrewowen <andrewowen@10.0.0.88>
2016-09-29 12:04:58 -0400

    SQL server version of materialized views for dimensional model

---
+++ booking-jail/r-load/OJBCJailBookingLoad/inst/raw/MaterializedViews-mssql.sql
@@ -0,0 +1,66 @@
+drop table if exists FullBookingView;
+
+create table FullBookingView as
+select
+    JailEpisode.JailEpisodeID,
+    BookingNumber,
+    EpisodeStartDate,
+    EpisodeEndDate,
+    isnull(JailEpisodeArrest.JailEpisodeArrestID, 99998) as JailEpisodeArrestID,
+    isnull(JailEpisodeCharge.JailEpisodeChargeID, 99998) as JailEpisodeChargeID,
+    isnull(JailEpisodeArrest.AgencyID, 99998) as ArrestAgencyID,
+    isnull(JailEpisodeCharge.AgencyID, 99998) as ChargeAgencyID,
+    isnull(ChargeClassTypeID, 99998) as ChargeClassTypeID,
+    isnull(BondStatusTypeID, 99998) as BondStatusTypeID,
+    isnull(BondTypeID, 99998) as BondTypeID,
+    isnull(ChargeTypeID, 99998)as  ChargeTypeID, 
+    isnull(ChargeDispositionTypeID, 99998) as ChargeDispositionTypeID, 
+    isnull(JurisdictionTypeID, 99998) as JurisdictionTypeID, 
+    BondAmount,
+    OccupationTypeID,
+    SexOffenderStatusTypeID,
+    FacilityID,
+    SupervisionUnitTypeID,
+    CaseStatusTypeID,
+    EpisodeStartDateID,
+    PopulationTypeID,
+    LengthOfStay,
+    OneYearRebooking,
+    PersonAgeTypeID,
+    PersonSexTypeID,
+    PersonRaceTypeID,
+    PersonEthnicityTypeID,
+    LanguageTypeID,
+    MilitaryServiceStatusTypeID,
+    DomicileStatusTypeID,
+    WorkReleaseStatusTypeID,
+    EducationLevelTypeID,
+    ProgramEligibilityTypeID,
+    isnull(BehavioralHealthEvaluationID, 99998) as BehavioralHealthEvaluationID,
+    isnull(BehavioralHealthTreatmentID, 99998) as BehavioralHealthTreatmentID,
+    isnull(MedicaidStatusTypeID, 99998) as MedicaidStatusTypeID,
+    isnull(InTreatmentAtBooking, 'N') as InTreatmentAtBooking,
+    BehavioralHealthAssessment.EndedDaysBeforeBooking,
+    isnull(BehavioralHealthEvaluationTypeID, 99998) as BehavioralHealthEvaluationTypeID,
+    isnull(MedicationTypeID, 99998) as MedicationTypeID,
+    isnull(AssessmentCategoryTypeID, 99998) as AssessmentCategoryTypeID,
+    isnull(TreatmentStatusTypeID, 99998) as TreatmentStatusTypeID,
+    isnull(TreatmentAdmissionReasonTypeID, 99998) as TreatmentAdmissionReasonTypeID,
+    isnull(TreatmentProviderTypeID, 99998) as TreatmentProviderTypeID,
+    SevereMentalIllnessIndicator,
+    BehavioralHealthTreatment.DaysBeforeBooking,
+    isnull(PrescribedMedicationID,99998) as PrescribedMedicationID,
+    iif(SixMonthRebooking='Y', 1, 0) * JailEpisode.JailEpisodeID as SixMonthRebookingCount, 
+    iif(OneYearRebooking='Y', 1, 0) * JailEpisode.JailEpisodeID as OneYearRebookingCount, 
+    iif(TwoYearRebooking='Y', 1, 0) * JailEpisode.JailEpisodeID as TwoYearRebookingCount, 
+    iif(IsActive='Y', 1, 0) * JailEpisode.JailEpisodeID as IsActiveBookingCount,
+    iif(InTreatmentAtBooking='Y', 1, 0) * JailEpisode.JailEpisodeID as InTreatmentAtBookingCount
+from
+    JailEpisode inner join Person on JailEpisode.PersonID=Person.PersonID
+    left join JailEpisodeArrest on JailEpisode.JailEpisodeID=JailEpisodeArrest.JailEpisodeID
+    left join JailEpisodeCharge on JailEpisodeArrest.JailEpisodeArrestID=JailEpisodeCharge.JailEpisodeArrestID
+    left join BehavioralHealthAssessment on BehavioralHealthAssessment.PersonID=Person.PersonID
+    left join BehavioralHealthEvaluation  on BehavioralHealthAssessment.BehavioralHealthAssessmentID=BehavioralHealthEvaluation.BehavioralHealthAssessmentID
+    left join BehavioralHealthAssessmentCategory on BehavioralHealthAssessment.BehavioralHealthAssessmentID=BehavioralHealthAssessmentCategory.BehavioralHealthAssessmentID
+    left join PrescribedMedication on BehavioralHealthAssessment.BehavioralHealthAssessmentID=PrescribedMedication.BehavioralHealthAssessmentID
+    left join BehavioralHealthTreatment  on BehavioralHealthAssessment.BehavioralHealthAssessmentID=BehavioralHealthTreatment.BehavioralHealthAssessmentID;
